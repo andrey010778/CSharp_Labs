7a412e9828d0f3d0ab0aa7d69bf7d68e0f685ca2
Андрей <7kuz@mail.ru>
2025-11-24 13:20:21 +0300

    Task sg

---
+++ Sg/Sg/Sg/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Sg
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            int number = 12345;
+            int sum = 0;
+
+            while (number > 0)
+            {
+                sum += number % 10;  
+                number /= 10;        
+            }
+
+            Console.WriteLine($"Сумма цифр: {sum}");
+
+        }
+    }
+}
+
